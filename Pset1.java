913d86018238a0af8b4ddac349fc1ad358c7323f
Trinity Moore <trinity_moore@m018460.aw.org>
2024-09-06 14:07:18 -0700

    Homework WS 1

---
+++ Pset1.java
@@ -0,0 +1,18 @@
+class Pset1{
+
+public static void main(String[] args) {
+	System.out.println("Trinity" + " is a student in IB CS");
+	
+}
+	
+
+}
+
+//2A The difference is Java doesn't have a set/certain purpose for
+//using, while python is mostly used "as a support language for 
+//software developers," (python.) Java is also calss based
+//and needs a main method while python doesnt need a main method
+
+
+
+
