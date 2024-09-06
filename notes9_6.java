2e5fb2af21adac2801493bedb0dd9179a36958f7
Trinity Moore <trinity_moore@m018460.aw.org>
2024-09-06 13:52:04 -0700

    uploading notes

---
+++ notes9_6.java
@@ -0,0 +1,34 @@
+class notes9_6 {
+
+	public static void main(String[] args) {
+	
+		//data in java needs to have a tyoe declared (say what data type
+		//it is)
+		int x = 5;
+		//with float w/o a decimal, java will add one (into 4.0)
+		float y = 4;
+		double d = 5.2;
+
+		//booleans are either true or false
+		boolean val = true;
+		char myCharacter = 'l';
+		//char needs single qutaion marks while string needs double quote
+		String name = "hellooo";
+
+		//After, need to print after witting and call back the variable
+		System.out.println (x + d);
+		System.out.println (y);
+		System.out.println (val);
+		System.out.println (myCharacter);
+		System.out.println (name);
+
+		//numerical data treats operators with marth 
+		//string data trats operatoers with concatenation
+
+		//java operatoers (+,-,*,/,%,++,--)
+		System.out.println(20 % 3);
+
+
+	}
+
+}
