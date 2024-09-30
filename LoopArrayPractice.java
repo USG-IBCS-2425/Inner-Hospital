b5f98a2341dacdccd0ee6a34296ead6342a822e4
Trinity Moore <trinity_moore@m018460.aw.org>
2024-09-30 10:57:51 -0700

    Homework so far

---
+++ LoopArrayPractice.java
@@ -0,0 +1,46 @@
+import java.util.*
+class LoopArrayPractice{
+	public static void main(String[] args) {
+		int a = {5, 4, 34, 2, 90, 50, 28, 34, 30, 28};
+		h = a.length;
+		String h = {"Green", "Fruit", "Drink", "Flood"};
+		numMatches ();
+		c = a.length;
+		String d = {};
+		int lengthfive = {3, 6, 7, 80, 90};
+		int lengthfive2 = {2, 64, 20, 49, 29};
+		inOrder 
+
+	}
+	public static void middleValue(int a){
+	y = a % 2; 
+		if (y==0){
+			return a[y];
+		}
+		else {
+			y = a +1;
+			return a;
+		}
+
+	}
+	public static void numMatches(String d, String h){
+		System.out.println(d.length);
+		int num = 0;
+	}
+
+	inOrder(int d){
+		if (d.length==5);
+			for (int i=0, i==d, i++){
+			if (0 == a[i] - a[i + 1]){
+				System.print.ln
+			}
+			else if (0< a[i] - a[ai +1])
+		}
+	}
+
+	doubleLetter(){
+		
+	}
+
+
+}
